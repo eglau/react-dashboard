4b31b2b63653b7708883eee8c82e716e028ab56d
Edward Lau <edwardglau@gmail.com>
2018-05-16 00:32:36 -0700

    added google sheets api to events page

--- src/Events.js
+++ src/Events.js
@@ -60,13 +60,12 @@ function EventBoxItem(props) {
         <p>
         <span><i className="fa fa-arrow-circle-right"></i> {event.location}</span>
         <br/>
-        <span>{event.notes}</span>
+        <span>{event.details}</span>
         </p>
       </div>
     </div>
   );
 }
-
 class Events extends React.Component {
   constructor(props) {
     super(props);
@@ -74,18 +73,68 @@ class Events extends React.Component {
       events: props.events
     }
   }
+  loadGoogleAPI() {
+    const that = this;
+    const GAPI_KEY = '???'; //load from local file
+
+    const script = document.createElement('script');
+    script.src = 'https://apis.google.com/js/api.js';
+    script.onload = () => {
+      const gapi = window.gapi;
+      gapi.load('client', () => {
+        gapi.client.setApiKey(GAPI_KEY);
+        gapi.client.load('https://sheets.googleapis.com/$discovery/rest?version=v4').then(() => {
+          gapi.client.sheets.spreadsheets.values.get({
+            spreadsheetId: '1odDYjVRSktN_5csh3xQIBQzME2ME4q6Gwj7Oj5lni7M',
+            range: 'A2:H',
+          }).then((response) => {
+            let eventsListing = [];
+            const today = new Date().setHours(0, 0, 0, 0);
+            response.result.values.filter((event) => {
+              const date = new Date(event[0]);
+              return (today <= date.setHours(0,0,0,0));
+            }).map((event) => {
+              //check if listing for date exists and get index if so
+              let listingIndex = -1;
+              for (let i = 0; i < eventsListing.length; i++) {
+                if (eventsListing[i].date === event[0]) {
+                  listingIndex = i;
+                }
+              }
+              //create listing if nonexistant
+              if (listingIndex === -1) {
+                listingIndex = eventsListing.length;
+                eventsListing.push({
+                  date: event[0],
+                  events: []
+                });
+              }
+              //add event to listing
+              eventsListing[listingIndex].events.push({
+                id: '123',
+                name: event[5],
+                time: `${event[1]} - ${event[2]}`,
+                attendance: event[3],
+                location: event[6],
+                details: event[7]
+              });
+            });
+            that.setState({
+              events: eventsListing,
+              isLoaded: true
+            })
+          }, (response) => {
+            that.setState({
+              error: response.result.error.message
+            });
+          });
+        });
+      });
+    };
+    document.body.appendChild(script);
+  }
   componentDidMount() {
-    let that = this;
-    axios('http://192.168.1.81:4000/events').then((response) => {
-      that.setState({
-        events: response.data,
-        isLoaded: true
-      });
-    }).catch((err) => {
-      that.setState({
-        error: 'Could not connect to Events database'
-      });
-    });
+    this.loadGoogleAPI();
   }
   render() {
     if (this.state.error) {
